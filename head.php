2930b23dd7c9dc6463f39a460889059a7db70cec
epact <105442264+repact@users.noreply.github.com>
2023-07-10 19:16:48 +0200

    Create head.php

---
+++ head.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<link rel="icon" href="assets/all/favicon.ico" sizes="any">
+	<link rel="icon" href="assets/all/favicon.svg" type="image/svg+xml">
+	<title>CORE</title>
+	<meta name="keywords" content="core, core fiesta,  fiesta, fiesta private server, private, server, online, game, fiesta online, fiesta na, fiesta eu, fiesta game, fiesta ps, ps fiesta, private server fiesta, MMORPG, Free-to-Play, Fantasy MMO, Online Game, fiesta gamigo, hunter kingdom, shine online, ハンターキングダム, ">
+	<meta name="description" content="The best Fiesta Online Private Server, which is created and maintained by professional developers. Don't miss the fun and join today!">
+
+
+
+	<link rel="stylesheet" href="https://unpkg.com/blaze-slider@latest/dist/blaze.css">
+
+
+	<link rel="stylesheet" href="assets/style.css">
+
+
+	<script src="https://unpkg.com/blaze-slider@latest/dist/blaze-slider.min.js" defer></script>
+	<script src="assets/script.js" defer></script>
+
+	<script  src="https://unpkg.com/@imacrayon/alpine-ajax" defer></script>
+
+	<script src="https://unpkg.com/@alpinejs/mask@3.x.x/dist/cdn.min.js" defer></script>
+	<script src="https://unpkg.com/@alpinejs/intersect@3.x.x/dist/cdn.min.js" defer></script>
+	<script src="https://unpkg.com/alpinejs" defer></script>
+
+
+
+
+</head>
